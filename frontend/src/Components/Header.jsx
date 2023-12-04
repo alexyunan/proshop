5743aed242387e21dc899d30fddb0e1e6bac9796
AlexYunan <alexandrosgiounan1@hotmail.com>
2023-12-04 13:50:26 +0200

    added react-router library

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,5 +1,6 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -7,21 +8,29 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>
-            <img src={logo} alt='ProShop' />
-            ProShop
-          </Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>
+              <img src={logo} alt='ProShop' />
+              ProShop
+            </Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ms-auto'>
-              <Nav.Link href='/cart' style={{ display: "flex", alignItems: "center" }}>
-                <FaShoppingCart style={{ marginRight: "0.2rem" }} />
-                Cart
-              </Nav.Link>
-              <Nav.Link href='/cart' style={{ display: "flex", alignItems: "center" }}>
-                <FaUser style={{ marginRight: "0.2rem" }} />
-                Sign in
-              </Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link style={{ display: "flex", alignItems: "center" }}>
+                  <FaShoppingCart style={{ marginRight: "0.2rem" }} />
+                  Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+                <Nav.Link style={{ display: "flex", alignItems: "center" }}>
+                  <FaUser style={{ marginRight: "0.2rem" }} />
+                  Sign in
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
