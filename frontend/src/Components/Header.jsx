7fcf021362f369aaf8eb5dbde01bd99af27e820d
Alexandros Giounan <alexandrosgiounan1@hotmail.com>
2023-12-17 17:29:30 +0200

    show number of cart items

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,33 +1,41 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Badge, Navbar, Nav, Container } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
 import { LinkContainer } from "react-router-bootstrap";
+import { useSelector } from "react-redux";
 import logo from "../assets/logo.png";
 
 const Header = () => {
+  const { cartItems } = useSelector((state) => state.cart);
+
   return (
     <header>
-      <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <LinkContainer to='/'>
+          <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
+              <img src={logo} alt="ProShop" />
               ProShop
             </Navbar.Brand>
           </LinkContainer>
 
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ms-auto'>
-              <LinkContainer to='/cart'>
-                <Nav.Link style={{ display: "flex", alignItems: "center" }}>
-                  <FaShoppingCart style={{ marginRight: "0.2rem" }} />
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart />
                   Cart
+                  {cartItems.length > 0 && (
+                    <Badge pill bg="success" style={{ marginLeft: "5px" }}>
+                      {cartItems.reduce((a, c) => a + c.qty, 0)}
+                    </Badge>
+                  )}
                 </Nav.Link>
               </LinkContainer>
 
-              <LinkContainer to='/login'>
-                <Nav.Link style={{ display: "flex", alignItems: "center" }}>
-                  <FaUser style={{ marginRight: "0.2rem" }} />
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser />
                   Sign in
                 </Nav.Link>
               </LinkContainer>
